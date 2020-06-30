cc6ae0ddc063f282c8f372d6ce2ec5b6ef65226c
Mika Le Gall <mika.legall@live.fi>
2020-06-30 23:05:54 +0300

    talteen

--- TelegramBotti/src/yoga/kulatantra/Main.java
+++ TelegramBotti/src/yoga/kulatantra/Main.java
@@ -3,7 +3,6 @@ package yoga.kulatantra;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
 public class Main {
 
--- TelegramBotti/src/yoga/kulatantra/ShalaRelaxBot.java
+++ TelegramBotti/src/yoga/kulatantra/ShalaRelaxBot.java
@@ -1,5 +1,7 @@
 package yoga.kulatantra;
 
+import java.util.List;
+//import java.util.Collections;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
@@ -7,50 +9,70 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class ShalaRelaxBot extends TelegramLongPollingBot {
 
+	List<ReplieAnswer> response = InitResponse.createReplies();
+
+	int i = 0;
+
 	@Override
 	public void onUpdateReceived(Update update) {
+//		Collections.shuffle(response);
 
 //		System.out.println("*************************   " + update + "   *************************");
 
+		String replieForString;
+		String replieForPhoto;
+		String replieForVideo;
+		String replieForDocument;
 		
+		if (i < response.size()) {
+			ReplieAnswer tempVariable = response.get(i);
+			i++;
+			replieForString = tempVariable.getResponseForString().replace("TELEGRAM_FirstName", update.getMessage().getFrom().getFirstName());
+			replieForPhoto  = tempVariable.getResponseForPhoto();
+			replieForVideo = tempVariable.getResponseForVideo();
+			replieForDocument = tempVariable.getResponseForDocument();
+		} else {
+			i = 0;
+			ReplieAnswer tempVariable = response.get(i);
+			i++;
+			replieForString = tempVariable.getResponseForString().replace("TELEGRAM_FirstName", update.getMessage().getFrom().getFirstName());
+			replieForPhoto  = tempVariable.getResponseForPhoto();
+			replieForVideo = tempVariable.getResponseForVideo();
+			replieForDocument = tempVariable.getResponseForDocument();
+			}
+
 		// We check if the update has a message and the message has text
 		if (update.hasMessage() && update.getMessage().hasText()) {
 			SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
-					String apu = update.getMessage().getText();
-					if (apu.equals("/start")) {
-					message
-					.setChatId(update.getMessage().getChatId())
-					.setText("Nyt voit kuvata minulle yhdellä sanalla sitä, mikä kuvastaa tätä kuluvaa hetkeä mielestäsi osuvimmin juuri nyt 🕉");			
-					} else {
-							message
-							.setChatId(update.getMessage().getChatId())
-							.setText("Kukkuluuruu, täällä ollaan! T: " + getBotUsername());
-//			        		.setText(update.getMessage().getText());
-					}
+			String tempVariable = update.getMessage().getText();
+			String tempVariableForName = update.getMessage().getFrom().getFirstName();
+			if (tempVariable.equals("/start")) {
+				message.setChatId(update.getMessage().getChatId()).setText(
+						"Nyt voit " + tempVariableForName + " kuvata minulle yhdellä sanalla sitä, mikä kuvastaa tätä kuluvaa hetkeä mielestäsi osuvimmin juuri nyt 🕉");
+			} else {
+				message.setChatId(update.getMessage().getChatId()).setText(replieForString);
+			}
+
 			try {
 				execute(message); // Call method to send the message
 			} catch (TelegramApiException e) {
 				e.printStackTrace();
 			}
-			apu=null;
+			tempVariable = null;
+			tempVariableForName = "";
 		}
-		
-		
+
 		// We check if the update has a message and onko kyseessä kuvaviesti
 		if (update.hasMessage() && update.getMessage().hasPhoto()) {
 			SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
 
-					if (update.getMessage().getCaption() != null) {
-					message		
-					.setChatId(update.getMessage().getChatId())
-					.setText("Ai sä lähetit mulle kuvan, kivaa! (paitsi, että toi sun laittama kuvaus '"
-					+ update.getMessage().getCaption()
-					+ "' oli vähän outo)");
-					} else {
-							message
-							.setChatId(update.getMessage().getChatId())
-							.setText("Ai sä lähetit mulle kuvan, kivaa!)");
-							}
+			if (update.getMessage().getCaption() != null) {
+				message.setChatId(update.getMessage().getChatId())
+						.setText("Lorem (foo '"
+								+ update.getMessage().getCaption() + "' bar)");
+			} else {
+				message.setChatId(update.getMessage().getChatId()).setText(replieForPhoto);
+			}
 			try {
 				execute(message); // Call method to send the message
 			} catch (TelegramApiException e) {
@@ -58,25 +80,21 @@ public class ShalaRelaxBot extends TelegramLongPollingBot {
 			}
 		}
 
-		
 		// We check if the update has a message and onko kyseessä video
 		if (update.hasMessage() && update.getMessage().hasVideo()) {
 			SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-					.setChatId(update.getMessage().getChatId())
-					.setText("Mä en kattele videoita");
+					.setChatId(update.getMessage().getChatId()).setText(replieForVideo);
 			try {
 				execute(message); // Call method to send the message
 			} catch (TelegramApiException e) {
 				e.printStackTrace();
 			}
 		}
-
 
 		// We check if the update has a message and onko kyseessä tiedosto
 		if (update.hasMessage() && update.getMessage().hasDocument()) {
 			SendMessage message = new SendMessage() // Create a SendMessage object with mandatory fields
-					.setChatId(update.getMessage().getChatId())
-					.setText("Älä lähettele mulle tiedostoja");
+					.setChatId(update.getMessage().getChatId()).setText(replieForDocument);
 			try {
 				execute(message); // Call method to send the message
 			} catch (TelegramApiException e) {
@@ -84,17 +102,15 @@ public class ShalaRelaxBot extends TelegramLongPollingBot {
 			}
 		}
 
-	
 	}
-
 
 	@Override
 	public String getBotUsername() {
-		return "Shala Relax";
+		return "@ShalaRelaxBot";
 	}
 
 	@Override
 	public String getBotToken() {
-		return "tantraOnAgamanEsoteerinenPuoli";
+		return "TantraOnAgamanEsoteerinenPuoli";
 	}
 }
